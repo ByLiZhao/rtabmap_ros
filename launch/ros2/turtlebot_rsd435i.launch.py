538aa7a9cdb162ba4278a78ac35d0d4260607aa2
John Z. Li <lizhao.johnlee@gmail.com>
2022-01-26 18:24:28 +0800

    add a new launch file for Turtlebot with Intel Realsense D435i.

---
+++ launch/ros2/turtlebot_rsd435i.launch.py
@@ -0,0 +1,87 @@
+# Requirements:
+#   Install Turtlebot3 packages
+#   Install https://github.com/mlherd/ros2_turtlebot3_waffle_intel_realsense
+# Example:
+#   $ export TURTLEBOT3_MODEL=waffle
+#   $ ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
+#
+#   SLAM:
+#   $ ros2 launch rtabmap_ros turtlebot3_rgbd.launch.py
+#   OR
+#   $ ros2 launch rtabmap_ros rtabmap.launch.py visual_odometry:=false frame_id:=base_footprint odom_topic:=/odom args:="-d" use_sim_time:=true rgb_topic:=/intel_realsense_r200_depth/image_raw depth_topic:=/intel_realsense_r200_depth/depth/image_raw camera_info_topic:=/intel_realsense_r200_depth/camera_info approx_sync:=true
+#
+#   Navigation (install nav2_bringup package):
+#     $ ros2 launch nav2_bringup navigation_launch.py use_sim_time:=True
+#     $ ros2 launch nav2_bringup rviz_launch.py
+#
+#   Teleop:
+#     $ ros2 run turtlebot3_teleop teleop_keyboard
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    qos = LaunchConfiguration('qos')
+    localization = LaunchConfiguration('localization')
+
+    parameters={
+          'frame_id':'base_footprint',
+          'use_sim_time':use_sim_time,
+          'subscribe_depth':True,
+          'use_action_for_goal':True,
+          'qos_image':qos,
+          'qos_imu':qos,
+          'Reg/Force3DoF':'true',
+          'Optimizer/GravitySigma':'0', # Disable imu constraints (we are already in 2D)
+          # 'compressed': True,    # use compressed images
+    }
+
+    remappings=[
+          ('rgb/image', '/camera/color/image_raw'),
+          ('rgb/camera_info', '/camera/color/camera_info'),
+          ('depth/image', '/camera/depth/image_rect_raw')]
+
+    return LaunchDescription([
+
+        # Launch arguments
+        DeclareLaunchArgument(
+            'use_sim_time', default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
+
+        DeclareLaunchArgument(
+            'qos', default_value='2',
+            description='QoS used for input sensor topics'),
+
+        DeclareLaunchArgument(
+            'localization', default_value='false',
+            description='Launch in localization mode.'),
+
+        # Nodes to launch
+
+        # SLAM mode:
+        Node(
+            condition=UnlessCondition(localization),
+            package='rtabmap_ros', executable='rtabmap', output='screen',
+            parameters=[parameters],
+            remappings=remappings,
+            arguments=['-d']), # This will delete the previous database (~/.ros/rtabmap.db)
+
+        # Localization mode:
+        Node(
+            condition=IfCondition(localization),
+            package='rtabmap_ros', executable='rtabmap', output='screen',
+            parameters=[parameters,
+              {'Mem/IncrementalMemory':'False',
+               'Mem/InitWMWithAllNodes':'True'}],
+            remappings=remappings),
+
+        Node(
+            package='rtabmap_ros', executable='rtabmapviz', output='screen',
+            parameters=[parameters],
+            remappings=remappings),
+    ])
